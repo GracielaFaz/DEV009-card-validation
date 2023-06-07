c4c46ebd40f48eaade45dc74d1f4d5faf1ee1bdd
GracielaFaz <graciefaz@gmail.com>
2023-06-07 10:03:57 -0600

    Cambios nuevos cambios nuevos

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import validator from './validator.js';
 
+const btnValidate = document.getElementById("validate");
+btnValidate.addEventListener("click",function(){
+    const creditCardNumber = document.getElementById("inputNumber").value; 
+    validator.isValid(creditCardNumber)
+    //validator.maskify(creditCardNumber)
+});
+
+
 console.log(validator);
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,45 @@
 const validator = {
-  // ...
+
+    
+    isValid: function(creditCardNumber){
+        let creditCardNumberReverse = creditCardNumber.split("").reverse();
+        console.log(creditCardNumberReverse);
+       /* let reverseLength = creditCardNumberReverse.length;
+        console.log(reverseLength)*/
+
+        for (let i = 0; i < creditCardNumberReverse.length; i++){
+           if((i+2) % 2 !== 0){
+            //se tienen que multiplicar * 2 y si el resultado es un numero de dos digitos se tienen que sumar esos dos digitos (ej. si es 13 = 1+3 = 4)
+            console.log(i);
+    
+           } else{
+            // sumar "impares"
+           }
+
+           // sumar "pares"
+         
+           
+        }
+
+        //al final los dos se tienen que sumar y se tiene que validar
+        /* if{
+            sum / 10 = entero (sum % 10 === 0) = True
+            sum / 10 = decimal (sum % 10 !=== 0) = False
+        } */
+
+        // 1 2 3 4 5 6
+        // 0 1 2 3 4 5
+
+    }
+    
+    
+/*
+
+maskify: function (){
+
+}
+*/
+  
 };
 
 export default validator;
