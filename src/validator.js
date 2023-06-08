6e728a4c3fdc05d4f297d2f0c4f5e0722e21f243
GracielaFaz <graciefaz@gmail.com>
2023-06-07 21:37:59 -0600

    Función isValid completa

--- src/validator.js
+++ src/validator.js
@@ -2,38 +2,40 @@ const validator = {
 
     
     isValid: function(creditCardNumber){
-        let creditCardNumberReverse = creditCardNumber.split("").reverse();
+        const creditCardNumberReverse = creditCardNumber.split("").reverse();
         console.log(creditCardNumberReverse);
-       /* let reverseLength = creditCardNumberReverse.length;
-        console.log(reverseLength)*/
+        let valueSum = 0;
+        let resultado
+       
 
         for (let i = 0; i < creditCardNumberReverse.length; i++){
-           if((i+2) % 2 !== 0){
-            //se tienen que multiplicar * 2 y si el resultado es un numero de dos digitos se tienen que sumar esos dos digitos (ej. si es 13 = 1+3 = 4)
-            console.log(i);
-    
-           } else{
-            // sumar "impares"
-           }
+            let pair = +creditCardNumberReverse[i];
+            
+            if((i+1) % 2 === 0){
+                pair *= 2;
+                if(pair > 9){
+                    pair -= 9;
+                }
+            }
 
-           // sumar "pares"
-         
-           
+            valueSum += pair;
+
+            resultado = valueSum % 10 === 0;
+            
+
         }
+    
+        console.log(resultado);
 
-        //al final los dos se tienen que sumar y se tiene que validar
-        /* if{
-            sum / 10 = entero (sum % 10 === 0) = True
-            sum / 10 = decimal (sum % 10 !=== 0) = False
-        } */
 
-        // 1 2 3 4 5 6
-        // 0 1 2 3 4 5
 
+    
+    //return valueSum % 10 === 0;
+    
+    
     }
-    
-    
-/*
+
+    /*
 
 maskify: function (){
 
@@ -43,3 +45,12 @@ maskify: function (){
 };
 
 export default validator;
+
+        //al final los dos se tienen que sumar y se tiene que validar
+        /* if{
+            sum / 10 = entero (sum % 10 === 0) = True
+            sum / 10 = decimal (sum % 10 !=== 0) = False
+        } */
+
+        // 1 2 3 4 5 6
+        // 0 1 2 3 4 5
