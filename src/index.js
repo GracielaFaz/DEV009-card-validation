e2e0baa0bd6e79a393e28479dd1790dc299986d3
GracielaFaz <graciefaz@gmail.com>
2023-06-08 11:14:30 -0600

    Fixed indentation errors

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import validator from './validator.js';
 
 const btnValidate = document.getElementById("validate");
 btnValidate.addEventListener("click",function(){
-    const creditCardNumber = document.getElementById("inputNumber").value; 
-    validator.isValid(creditCardNumber)
-    //validator.maskify(creditCardNumber)
+  const creditCardNumber = document.getElementById("inputNumber").value; 
+  validator.isValid(creditCardNumber)
+  //validator.maskify(creditCardNumber)
 });
 
 
--- src/validator.js
+++ src/validator.js
@@ -1,31 +1,32 @@
 const validator = {
-
-    
-    isValid: function(creditCardNumber){
-        const creditCardNumberReverse = creditCardNumber.split("").reverse();
-        console.log(creditCardNumberReverse);
-        let valueSum = 0;
-        let resultado
+  isValid: function(creditCardNumber){
+    const creditCardNumberReverse = creditCardNumber.split("").reverse();
+    console.log(creditCardNumberReverse);
+    let valueSum = 0;
+    let resultado
        
-
-        for (let i = 0; i < creditCardNumberReverse.length; i++){
-            let pair = +creditCardNumberReverse[i];
+    for (let i = 0; i < creditCardNumberReverse.length; i++){
+      let pair = +creditCardNumberReverse[i];
             
-            if((i+1) % 2 === 0){
-                pair *= 2;
-                if(pair > 9){
-                    pair -= 9;
-                }
-            }
-
-            valueSum += pair;
-
-            resultado = valueSum % 10 === 0;
-            
-
+      if((i+1) % 2 === 0){
+        pair *= 2;
+        if(pair > 9){
+          pair -= 9;
         }
-    
-        console.log(resultado);
+        valueSum += pair; 
+      } else {
+        valueSum += pair;
+      }
+
+           
+
+      resultado = valueSum % 10 === 0;
+            
+
+    }
+
+    //return resultado;
+    console.log(resultado);
 
 
 
@@ -33,9 +34,9 @@ const validator = {
     //return valueSum % 10 === 0;
     
     
-    }
+  }
 
-    /*
+  /*
 
 maskify: function (){
 
@@ -46,11 +47,11 @@ maskify: function (){
 
 export default validator;
 
-        //al final los dos se tienen que sumar y se tiene que validar
-        /* if{
-            sum / 10 = entero (sum % 10 === 0) = True
-            sum / 10 = decimal (sum % 10 !=== 0) = False
-        } */
-
-        // 1 2 3 4 5 6
-        // 0 1 2 3 4 5
+//al final los dos se tienen que sumar y se tiene que validar
+/* if{
+  sum / 10 = entero (sum % 10 === 0) = True
+   sum / 10 = decimal (sum % 10 !=== 0) = False
+} */
+    
+// 1 2 3 4 5 6
+// 0 1 2 3 4 5
